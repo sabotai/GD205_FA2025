82703d059126edee9ff6480c4cbe82f5d0c57825
Juno AM <sabotaione@gmail.com>
2025-10-22 19:00:21 -0400

    added week 7 stuff

--- Assets/Scripts/SubMove.cs
+++ Assets/Scripts/SubMove.cs
@@ -5,7 +5,8 @@ public class SubMove : MonoBehaviour
     //Rigidbody allows for independent physics movement with the physics engine
     Rigidbody myRb; //create a new local Rigidbody called myRb
     //public Rigidbody publicRb;
-
+    public AudioClip boomClip;
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -42,12 +43,29 @@ public class SubMove : MonoBehaviour
 
         if (Input.GetKey("q"))
         {
-            myRb.AddForce(0f, 1f, 0f); //add force to our rigidbody in this direction
+            myRb.AddForce(0f, -1f, 0f); //add force to our rigidbody in this direction
         }
 
         if (Input.GetKey("e"))
         {
-            myRb.AddForce(0f, -1f, 0f); //add force to our rigidbody in this direction
+            myRb.AddForce(0f, 1f, 0f); //add force to our rigidbody in this direction
         }
+
+        //create a brake using the spacebar
+        if (Input.GetKey("space")){
+            myRb.linearVelocity *= 0.99f; //decrease the velocity by 1% each loop
+        }
+    }
+    //this function is called whenever this gameobject collides with another one
+    //at least one of the objects must have a rigidbody
+    void OnCollisionEnter(Collision colReport) //it creates a new Collision object with the info about the collision, similar to the police report when jeremy hit yeyzer
+    {
+        Debug.Log("collision you died :)"); //a message in the console showing that this is working
+        
+        //use getcomponent to access the audiosource attached to the same gameobject
+        //then access the playoneshot method, which will play an audioclip from the audiosource one time
+        //the first parameter is which audioclip
+        //the second is the volume percentage represented by a decimal
+        GetComponent<AudioSource>().PlayOneShot(boomClip, .95f);
     }
 }
