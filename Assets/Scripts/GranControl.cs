449d6d2fd544354f81a8bb4b0f5242b12dc69bbb
Juno AM <sabotaione@gmail.com>
2025-11-05 18:44:56 -0500

    added functionality for an additional action for the character controller

--- Assets/Scripts/GranControl.cs
+++ Assets/Scripts/GranControl.cs
@@ -13,12 +13,22 @@ public class GranControl : MonoBehaviour
     {
         //use the SetFloat method to change the "Move" parameter to a default value of 0 as the default state
         GetComponent<Animator>().SetFloat("Move", 0f);
+        
+        //change the "Jump Attack" parameter to have a value of false by default
+        GetComponent<Animator>().SetBool("Jump Attack", false);
 
         //if the player is pressing the w key currently
         if (Input.GetKey(KeyCode.W))
         {
             //change the "Move" parameter to have a value of 1 when you're pressing w
             GetComponent<Animator>().SetFloat("Move", 1f);
+
+        }
+        //if the player presses the space bar down once
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //change the "Jump Attack" parameter to have a value of true when you're pressing space
+            GetComponent<Animator>().SetBool("Jump Attack", true);
 
         }
     }
